fb78104aa6b6c305265a71cb3ff57a99ef014d96
mdyasinahmed <yasinahmed7875@gmail.com>
2024-07-29 12:05:59 +0600

    add app page updates

--- easyload/lib/home_page.dart
+++ easyload/lib/home_page.dart
@@ -3,26 +3,37 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Enter YouTube URL',
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('YouTube Downloader'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter YouTube URL',
+                ),
               ),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                // Add download logic here
-              },
-              child: Text('Download'),
-            ),
-          ],
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white, backgroundColor: Color(0xFFFF2147),    // Text color
+                  padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                  textStyle: TextStyle(fontSize: 16),
+                ),
+                onPressed: () {
+                  // Add download logic here
+                },
+                child: Text('Download'),
+              ),
+            ],
+          ),
         ),
       ),
     );
